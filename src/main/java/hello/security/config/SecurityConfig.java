1edc0ac24490757eb9a2e3af1c8a2b5f6e72698b
Flre-fly <66842566+Flre-fly@users.noreply.github.com>
2023-02-15 18:03:05 +0900

    security설정 - 이제 더이상 login으로 들어가도 시큐리티가 낚아채서 로그인화면을 보여주지않음

---
+++ src/main/java/hello/security/config/SecurityConfig.java
@@ -0,0 +1,28 @@
+package hello.security.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.SecurityBuilder;
+import org.springframework.security.config.annotation.web.WebSecurityConfigurer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+@EnableWebSecurity//스프링 시큐리터필터(Security Config클래스)가 스프링필터체인에 등록된다
+public class SecurityConfig{
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.csrf().disable();
+        http.authorizeRequests()
+                .antMatchers("/user/**").authenticated()
+                .antMatchers("/manager/**").access("hasAnyRole('ROLE_MANAGER','ROLE_ADMIN')")
+                .antMatchers("/admin/**").access("hasRole('ROLE_ADMIN')")
+                .anyRequest().permitAll();
+
+        return http.build();
+    }
+
+}
--- src/main/java/hello/security/controller/IndexController.java
+++ src/main/java/hello/security/controller/IndexController.java
@@ -2,11 +2,42 @@ package hello.security.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class IndexController {
     @GetMapping
     public String index(){
         return "index";
+    }
+    @GetMapping("/user")
+    @ResponseBody
+    public String user(){
+        return "user";
+    }
+    @GetMapping("/manager")
+    @ResponseBody
+    public String manager(){
+        return "manager";
+    }
+    @GetMapping("/admin")
+    @ResponseBody
+    public String admin(){
+        return "admin";
+    }
+    @GetMapping("/join")
+    @ResponseBody
+    public String join(){
+        return "join";
+    }
+    @GetMapping("/login")
+    @ResponseBody
+    public String login(){
+        return "login";
+    }
+    @GetMapping("/joinComplete")
+    @ResponseBody
+    public String joinComplete(){
+        return "joinComplete";
     }
 }
